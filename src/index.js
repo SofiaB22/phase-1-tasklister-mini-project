3ef8c6715f7c4199992c8e79ae6940ea38d075e1
+Bridgette <+sofiaeuc112002@gmail.com+>
2023-12-08 17:25:26 -0600

    lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  <script src=".scr/index.js"></script>
+  const checkbox = document.querySelector("#id-checkbox");
+  checkbox.addEventListener("click", checkboxClick, false);
+  function checkboxClick(event) {
+    let warn ="preventDefault() won't let you check this!<br>";
+    document.getElementById("output-box").innerHTML += warn;
+    event.preventDefault();
+  }
 });
